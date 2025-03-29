bf124582dba84d45e8b71d82ef56615175c17b75
het <hetvyas0511@gmail.com>
2025-03-29 20:35:58 +0530

    complited 1st draft of website

---
+++ scipt.js
@@ -0,0 +1,7 @@
+        function openGoogleMaps() {
+            window.open('https://www.google.com/maps?q=Indus+University,+Rancharda,+Via,+Shilaj,+Gujarat+382115', '_blank');
+        }
+
+        function startNavigation() {
+            window.open('https://www.google.com/maps/dir/?api=1&destination=Indus+University,+Rancharda,+Via,+Shilaj,+Gujarat+382115', '_blank');
+        }
